214350c99ad789af35b5cf236b0c024c0370a6c5
MeetBudheliya <meetbudheliya04@gmail.com>
2020-10-26 19:24:38 +0530

    commit

---
+++ CollectionViewsPage/CollectionViewCell1.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell1.swift
+//  CollectionViewsPage
+//
+//  Created by MAC on 26/10/20.
+//
+
+import UIKit
+
+class CollectionViewCell1: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+}
--- CollectionViewsPage/ViewController.swift
+++ CollectionViewsPage/ViewController.swift
@@ -10,24 +10,43 @@ import UIKit
 class ViewController: UIViewController , UICollectionViewDelegate ,UICollectionViewDataSource{
 
     @IBOutlet weak var Collection1: UICollectionView!
+    @IBOutlet weak var Collection2: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     var imgList = [UIImage]()
     var nameList = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        imgList = [#imageLiteral(resourceName: "Simulator Screen Shot - iPhone 8 Plus - 2020-10-19 at 09.44.15"),#imageLiteral(resourceName: "zhang-kaiyv-oc9mA8_aL9Q-unsplash"),#imageLiteral(resourceName: "toa-heftiba-m7r1BVoWTAk-unsplash"),#imageLiteral(resourceName: "clay-banks-tYsFbfQp4_A-unsplash"),#imageLiteral(resourceName: "Simulator Screen Shot - iPhone 8 Plus - 2020-10-19 at 09.44.15")]
-        nameList = ["A","B","C","D","E"]
+        imgList = [#imageLiteral(resourceName: "jonny-caspari-A7ol2HfnycY-unsplash"),#imageLiteral(resourceName: "ivan-rohovchenko-vpQHhRdoadU-unsplash"),#imageLiteral(resourceName: "clay-banks-tYsFbfQp4_A-unsplash"),#imageLiteral(resourceName: "makm-photography-u0irC_UnbOk-unsplash"),#imageLiteral(resourceName: "jim-cooke-r6g4GcD6lP0-unsplash")]
+        nameList = ["Castle","Amazing Historical place","Road","House","Fort"]
+        pageControl.numberOfPages = nameList.count
         
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return imgList.count
     }
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        pageControl.currentPage = indexPath.row
+    }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = Collection1.dequeueReusableCell(withReuseIdentifier: "Cell1", for: indexPath) as! CollectionViewCell
-        cell.image.image = imgList[indexPath.row]
-        cell.label.text = nameList[indexPath.row]
-        return cell
+        pageControl.currentPage = indexPath.row
+        if collectionView == Collection1
+        {
+            let cell = Collection1.dequeueReusableCell(withReuseIdentifier: "Cell1", for: indexPath) as! CollectionViewCell
+            cell.image.image = imgList[indexPath.row]
+            cell.label.text = nameList[indexPath.row]
+            cell.image.layer.cornerRadius = 15
+            return cell
+        }
+        else
+        {
+            let cell = Collection2.dequeueReusableCell(withReuseIdentifier: "Cell2", for: indexPath) as! CollectionViewCell1
+            cell.image.image = imgList[indexPath.row]
+            cell.image.layer.cornerRadius = 10
+            return cell
+        }
+
     }
 
 }
