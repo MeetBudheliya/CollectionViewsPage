5a4c166f7e85b32ee6d68cca85cb24a7c91713f9
MeetBudheliya <meetbudheliya04@gmail.com>
2020-10-26 11:41:39 +0530

    commit

---
+++ CollectionViewsPage/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewsPage
+//
+//  Created by MAC on 26/10/20.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+}
--- CollectionViewsPage/ViewController.swift
+++ CollectionViewsPage/ViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UICollectionViewDelegate ,UICollectionViewDataSource{
 
+    @IBOutlet weak var Collection1: UICollectionView!
+    
+    var imgList = [UIImage]()
+    var nameList = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imgList = [#imageLiteral(resourceName: "Simulator Screen Shot - iPhone 8 Plus - 2020-10-19 at 09.44.15"),#imageLiteral(resourceName: "zhang-kaiyv-oc9mA8_aL9Q-unsplash"),#imageLiteral(resourceName: "toa-heftiba-m7r1BVoWTAk-unsplash"),#imageLiteral(resourceName: "clay-banks-tYsFbfQp4_A-unsplash"),#imageLiteral(resourceName: "Simulator Screen Shot - iPhone 8 Plus - 2020-10-19 at 09.44.15")]
+        nameList = ["A","B","C","D","E"]
+        
     }
-
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imgList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = Collection1.dequeueReusableCell(withReuseIdentifier: "Cell1", for: indexPath) as! CollectionViewCell
+        cell.image.image = imgList[indexPath.row]
+        cell.label.text = nameList[indexPath.row]
+        return cell
+    }
 
 }
 
